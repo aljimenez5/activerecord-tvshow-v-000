be974920b4106985c5909feed6335c305673ec88
Arvida Jimenez <arvida.jimenez@gmail.com>
2018-09-15 14:47:17 +0000

    automatically backed up by learn

--- db/migrate/001_create_shows.rb
+++ db/migrate/001_create_shows.rb
@@ -1,7 +1,11 @@
 class CreateShows < ActiveRecord::Migration
   
   def change
-    create_table :shows   
+    create_table :shows do |t|
+      t.string
+      t.string
+      t.string
+      
     
   end
   
